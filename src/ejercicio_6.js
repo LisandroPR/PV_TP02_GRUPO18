c11dcfbdb3e7286085010a5cb98d645830d544d0
guanuco96 <guanucocarlos2@gmail.com>
2025-04-14 12:54:36 -0300

    modificados

---
+++ src/ejercicio_6.js
@@ -0,0 +1,5 @@
+let horas= 2;
+let minutos= 40;
+let totalsegundos = (horas * 3600) + (minutos * 60);
+
+alert("horas: " + horas + " " + "minutos: " + minutos + " " + "total en segundos: " + totalsegundos);
