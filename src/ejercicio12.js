b60efc2fe161ed90175132c9039c68523c1f5343
Lisandro <lisandroperezruarte@gmail.com>
2025-04-21 18:55:18 -0300

    Ejercicios 4, 19, 23, Lisandro Perez Ruarte

--- src/ejercicio12.js
+++
@@ -1,7 +0,0 @@
-// Ejercicio 12: Escribir un programa que pida un número y determine si es par o impar.
-let numero = parseInt(prompt("Ingrese un número:"));
-if (numero % 2 === 0) {
-    alert("El número " + numero + " es par.");
-} else {
-    alert("El número " + numero + " es impar.");
-}
--- src/ejercicio5.js
+++
@@ -1,7 +0,0 @@
-let horas = 4; 
-let minutos = 37; 
-let segundos = (horas * 3600) + (minutos * 60);
-
-console.log("Horas: " + horas);
-console.log("Minutos: " + minutos);
-console.log("Total en segundos: " + segundos);
