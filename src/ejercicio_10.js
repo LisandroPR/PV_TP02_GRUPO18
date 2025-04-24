c411c19265ae8b3a043bdb3e377d331e239e3628
Pablodan7 <pablodan7.pdg@gmail.com>
2025-04-24 18:16:07 -0300

    Ejercicios 2,5,8,12,17,20,24 Pablo Garzón

--- src/ejercicio_10.js
+++
@@ -1,9 +0,0 @@
-//Solicitar al usuario que ingrese tres números enteros, luego mostrar como resultado el promedio de los tres. El promedio de tres números se calcula sumando los tres números y dividiendo entre 3.
-
-let num1 = parseInt(prompt("Ingrese el primer numero: "));
-let num2 = parseInt(prompt("Ingrese el segundo numero: "));
-let num3 = parseInt(prompt("Ingrese el tercer numero: "));
-
-let promedio = (num1 + num2 + num3) / 3;
-
-alert("El promedio de los tres numeros es: " + promedio);
